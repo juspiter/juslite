2fb723d5a245792840360b401632895b429c6b12
rkrocha <rafaelkr7@gmail.com>
2021-10-29 17:36:27 -0300

    page now gets info from json file

--- page/src/App.js
+++ page/src/App.js
@@ -1,43 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import GetLocalJSON from './components/GetLocalJSON';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component
+{
+  render()
+  {
+    return (
+      <div className="App">
       <header className="App-header">
         <h1>JusLite</h1>
         <h2>TJAL - 0710802-55.2018.8.02.0001</h2>
         <h3>MOVIMENTAÇÕES</h3>
-        <table>
-          <tr>
-            <th>Data</th>
-            <th>Movimento</th>
-          </tr>
-          <tr>
-            <td>22/02/2021</td>
-            <td>Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso</td>
-          </tr>
-          <tr>
-            <td>10/02/2021</td>
-            <td>Juntada de Documento
-Nº Protocolo: WMAC.21.70031538-2 Tipo da Petição: Contrarrazões Data: 10/02/2021 19:27</td>
-          </tr>
-        </table>
+        <GetLocalJSON/>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
-{/* <table>
-  <tr>
-    <th>Month</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <td>January</td>
-    <td>$100</td>
-  </tr>
-</table> */}
---
+++ page/src/components/GetLocalJSON.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import process from './template.json'; // RENAME VAR
+class GetLocalJSON extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            process :process
+        };
+    }
+
+    render() {
+        const {process} = this.state;
+        return(
+            <div>
+                <ol className="item">
+                {
+                    process.map(proc => (
+                        <div>
+							<p className="title">{proc['changes'][0]['date']}   {proc['changes'][0]['title']}</p>
+                            <p className="body">{proc['changes'][0]['content']}</p>
+							<br></br>
+							<p className="title">{proc['changes'][1]['date']}   {proc['changes'][1]['title']}</p>
+                            <p className="body">{proc['changes'][1]['content']}</p>
+                          </div>
+                    ))
+                }
+                </ol>
+            </div>
+        );
+    }
+  }
+
+  export default GetLocalJSON;
