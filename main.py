f3d6d45f12ee9ebf2bc536b6d44629a0769b9448
rkrocha <rafaelkr7@gmail.com>
2021-10-29 20:02:31 -0300

    uncluttered main

--- main.py
+++ main.py
@@ -1,45 +1,4 @@
-from pymongo import MongoClient
 from storage import Storage
 
 
-# coll = MongoClient('172.17.0.3:27017').jusdb.lawsuits
-
-# coll.insert_one({
-# 		"number": "0710802-55.2018.8.02.0001",
-# 		"changes":
-# 		[
-# 			{
-# 				"date": "22/02/2021",
-# 				"title": "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso",
-# 				"content": ""
-# 			},
-# 			{
-# 				"date": "10/02/2021",
-# 				"title": "Juntada de Documento",
-# 				"content": "Nº Protocolo: WMAC.21.70031538-2 Tipo da Petição: Contrarrazões Data: 10/02/2021 19:27"
-# 			}
-# 		]
-# })
-
-store = Storage('172.17.0.3:27017')
-# store.add_lawsuit({
-# 		"number": "0710802-55.2018.8.02.0001",
-# 		"changes":
-# 		[
-# 			{
-# 				"date": "22/02/2021",
-# 				"title": "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso",
-# 				"content": ""
-# 			},
-# 			{
-# 				"date": "10/02/2021",
-# 				"title": "Juntada de Documento",
-# 				"content": "Nº Protocolo: WMAC.21.70031538-2 Tipo da Petição: Contrarrazões Data: 10/02/2021 19:27"
-# 			}
-# 		]
-# })
-
-store.delete_lawsuit("0710802-55.2018.8.02.0001")
-
-for lawsuit in store.conn.jusdb.lawsuits.find():
-    print(lawsuit)
+storage = Storage('172.17.0.3:27017')
