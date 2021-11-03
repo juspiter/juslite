2e05f325d4420c43ffeae433eb05fbc4605175a3
rkrocha <rafaelkr7@gmail.com>
2021-11-03 17:55:10 -0300

    further code cleanup and organization

--- parsecrawler.py
+++ parsecrawler.py
@@ -1,24 +1,26 @@
-import re
-
-
 def parse_crawl(soup):
     lawsuit = {}
     changes = []
-    lawsuit.update({"number": soup.find(id="numeroProcesso").get_text().strip()})
-    move_table = soup.find(id="tabelaTodasMovimentacoes")
+
+    lawsuit.update({"number": soup.find(
+        id="numeroProcesso").get_text(strip=True)})
+
+    move_table = soup.find('tbody', id="tabelaTodasMovimentacoes")
     moves = move_table.find_all('tr')
 
     for move in moves:
         change = {}
+
         change.update({"date": move.find(
-            class_="dataMovimentacao").get_text().strip()})
-        desc = re.sub('[\t]+', ' ', move.find(
-            class_="descricaoMovimentacao").get_text())
-        desc = desc.strip().splitlines()
-        desc = list(filter(None, desc))
-        desc[:] = [item for item in desc if item != ' ']
+            'td', class_="dataMovimentacao").get_text(strip=True)})
+
+        desc = move.find(
+            'td', class_="descricaoMovimentacao").get_text('\n', strip=True)
+        desc = desc.splitlines()
+
         change.update({"title": desc[0]})
-        change.update({"content": " ".join(desc[1:])})
+        change.update({"content": ' '.join(desc[1:])})
+
         changes.append(change)
 
     lawsuit.update({"changes": changes})
