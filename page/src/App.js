98a9136b9d9b5aac05f156404c7c1813bc9756aa
rkrocha <rafaelkr7@gmail.com>
2021-11-04 20:10:18 -0300

    page implementation in progress

--- page/src/App.js
+++ page/src/App.js
@@ -1,20 +1,72 @@
 //import logo from './logo.svg';
 import React, { Component } from 'react';
-import GetLocalJSON from './components/GetLocalJSON';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useState } from "react";
+import display from "./pages/display";
+// import GetLocalJSON from './components/GetLocalJSON';
+// import './App.css';
 
-class App extends Component
+//class SearchBox extends Component
+//{
+//  prop_process_id;
+//}
+
+// class SearchBox extends Component
+// {
+//   constructor(props)
+//   {
+//     super(props);
+//     this.state = {number_process: ''}
+//     //const[number, print_number] = useState('')
+//   }
+//   function print_number(params) {
+//     return(
+//       <div>number_process</div>
+//       <button type="submit" onClick={print_number}>Buscar</button>
+//     );
+//   }
+//   render()
+//   {
+//     return (
+//       <div>
+//         <input placeholder="Digite o numero do processo"/>
+//       </div>
+//     );
+//   }
+// }
+// <div>
+// <input
+//    type="text"
+//    placeholder="Search here"
+//    onChange={handleChange}
+//    value={searchInput} />
+
+function App()
 {
-  render()
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    // alert(`The name you entered was: ${name}`)
+      <Route path="/display">
+          <display />
+        </Route>
+  }
   {
     return (
       <div className="App">
-      <header className="App-header">
-        <h1>JusLite</h1>
-        <h2>TJAL - 0710802-55.2018.8.02.0001</h2>
-        <h3>MOVIMENTAÇÕES</h3>
-        <GetLocalJSON/>
-      </header>
+        <header>
+          <h1>Juslite</h1>
+        </header>
+        <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search here"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- page/src/components/GetLocalJSON.js
+++ page/src/components/GetLocalJSON.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import process from './template.json'; // RENAME VAR
+import process from './lawsuit.json'; // RENAME VAR
 class GetLocalJSON extends Component {
     constructor(props){
         super(props);
@@ -20,7 +20,7 @@ class GetLocalJSON extends Component {
 							<p className="title">{proc['changes'][0]['date']}   {proc['changes'][0]['title']}</p>
                             <p className="body">{proc['changes'][0]['content']}</p>
 							<br></br>
-							<p className="title">{proc['changes'][1]['date']}   {proc['changes'][1]['title']}</p>
+                            <p className="title">{proc['changes'][1]['date']}   {proc['changes'][1]['title']}</p>
                             <p className="body">{proc['changes'][1]['content']}</p>
                           </div>
                     ))
