b4e577d1e75c63e97c10f414e2004438130bf004
rkrocha <rafaelkr7@gmail.com>
2021-11-08 18:04:29 -0300

    added api.py

---
+++ api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from storage import Storage
+import json
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+storage = Storage('172.17.0.2:27017')
+
+
+@app.route("/lawsuit/<path:endpoint>", methods=["GET"])
+@cross_origin()
+def query_lawsuit(endpoint):
+    return str(storage.get_lawsuit(endpoint))
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=3001)
