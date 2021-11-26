f75ecc8798bfb6fef8d94f730780fc4b07b3be0c
rkrocha <rafaelkr7@gmail.com>
2021-11-26 19:08:58 -0300

    added module suitparser

---
+++ suitparser.py
@@ -0,0 +1,31 @@
+import datetime
+
+class SuitParser:
+    def __init__(self, number: str):
+        self.number = number
+        self.is_valid = self.check_number(self.number)
+        if self.is_valid is False:
+            return
+        self.formatted_number = self.format_number(self.number)
+
+    def check_number(self, number: str) -> bool:
+        raw_num = number.translate({ord(c): None for c in '.-'})
+        if (len(raw_num) != 20 or not
+            raw_num.isdigit() or not
+            self.court_is_valid(raw_num) or not
+            self.year_is_valid(raw_num)):
+            return False
+        self.number = raw_num # substituir por regex
+        return True
+
+    def court_is_valid(self, raw_num: str) -> bool:
+        if raw_num[13] == '8' and raw_num[14:16] in ['02', '06']:
+            return True
+        return False
+
+    def year_is_valid(self, raw_num: str) -> bool:
+        current_year = datetime.datetime.now().year
+        return True if current_year >= int(raw_num[9:13]) else False
+
+    def format_number(self, n: str) -> str:
+        return f"{n[:7]}-{n[7:9]}.{n[9:13]}.{n[13]}.{n[14:16]}.{n[16:]}"
---
+++ suitparserunittest.py
@@ -0,0 +1,51 @@
+from suitparser import SuitParser
+import unittest
+
+class TestSuitParser(unittest.TestCase):
+    def test_00_all_is_number(self):
+        str_num = "0000001-02.2017.8.02.0149"
+        ret = SuitParser(str_num)
+        self.assertTrue(ret.is_valid)
+
+    def test_01_receives_formatted_number_and_returns_without_change(self):
+        str_num = "0000001-02.2017.8.02.0149"
+        ret = SuitParser(str_num)
+        self.assertEqual(str_num, ret.formatted_number)
+
+    def test_02_return_true_when_number_is_valid(self):
+        str_num = "0000001-02.2017.8.02.0149"
+        ret = SuitParser(str_num)
+        self.assertTrue(ret.is_valid)
+
+    def test_03_return_true_when_number_is_valid_but_not_formatted(self):
+        str_num = "00000010220178020149"
+        ret = SuitParser(str_num)
+        self.assertTrue(ret.is_valid)
+
+    def test_04_return_false_when_number_is_shorter_than_valid(self):
+        str_num = "666" # roque _|m/ 😈
+        ret = SuitParser(str_num)
+        self.assertFalse(ret.is_valid)
+
+    def test_05_return_format_number_from_raw_number(self):
+        str_num = "00000010220178020149"
+        ret = SuitParser(str_num)
+        formatted_number = ret.formatted_number
+        self.assertEqual("0000001-02.2017.8.02.0149", formatted_number)
+
+    def test_06_return_false_because_there_are_letters(self):
+        str_num = "0710x02-55.2018.8.02.0001"
+        ret = SuitParser(str_num)
+        self.assertFalse(ret.is_valid)
+
+    def test_07_return_false_if_court_is_not_supported(self):
+        ret = SuitParser("0000001-02.2017.6.02.0149")
+        self.assertFalse(ret.is_valid)
+
+    def test_08_return_false_if_court_is_not_supported2(self):
+        ret = SuitParser("0000001-02.2017.8.03.0149")
+        self.assertFalse(ret.is_valid)
+
+    def test_09_return_false_if_future_predicion(self):
+        ret = SuitParser("0000001-02.2077.8.02.0149")
+        self.assertFalse(ret.is_valid)
