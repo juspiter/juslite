26befeb2d3eec1d5356318c0026e3092c731eed3
rkrocha <rafaelkr7@gmail.com>
2022-02-01 19:45:31 -0300

    made adjustments and cleaned up About section

--- page/src/components/AboutContent.js
+++ page/src/components/AboutContent.js
@@ -10,7 +10,7 @@ const AboutContent = () => {
         <div class="carousel-item active">
           <div class="_contents">
             <h5 class="text-center">Sobre</h5>
-            <p>O Juslite é um site gratuito e open-source que agrega informações públicas de tribunais brasileiros. É um projeto fruto de colaboração entre a <a href="https://www.42sp.org.br/">42 São Paulo</a> e o <a href="https://www.jusbrasil.com.br/">Jusbrasil</a>.</p>
+            <p>O Juslite é um site gratuito e de <a href="https://github.com/juspiter/juslite">código aberto</a> para consulta processual, que agrega informações públicas de tribunais brasileiros. É um projeto fruto de colaboração entre a <a href="https://www.42sp.org.br/">42 São Paulo</a> e o <a href="https://www.jusbrasil.com.br/">Jusbrasil</a>.</p>
           </div>
         </div>
         <div class="carousel-item">
@@ -59,32 +59,3 @@ const AboutContent = () => {
 }
 
 export default AboutContent;
-
-{/* <div className="container">
-      <div className="row align-items-start">
-        <div className="col-3 m-4 card">
-          <h5>Sobre</h5>
-          <p>O Juslite é um site gratuito e open-source que agrega informações públicas de tribunais brasileiros. É um projeto fruto de colaboração entre a <a href="https://www.42sp.org.br/">42 São Paulo</a> e o <a href="https://www.jusbrasil.com.br/">Jusbrasil</a>.</p>
-        </div>
-        <div className="col-3 m-4 card">
-          <h5>Instruções</h5>
-          <p>Digite a sua pesquisa na caixa de busca, sendo que qualquer termo digitado será buscado entre nomes de tribunais, números de processos, partes envolvidas, magistrados, assunto, classe, foro e vara.</p>
-          <p>Tribunais suportados: TJAL, TJCE, TST.</p>
-        </div>
-        <div className="col-3 m-4 card">
-          <h5>Autoria</h5>
-          <h6>Desenvolvedores:</h6>
-          <ul>
-            <li><a href="https://github.com/aalcara">Anderson</a></li>
-            <li><a href="https://github.com/LuccasRMedeiros">Luccas</a></li>
-            <li><a href="https://github.com/natflausino">Natasha</a></li>
-            <li><a href="https://github.com/rkrocha">Rafael</a></li>
-          </ul>
-          <h6>Apoio e agradecimentos:</h6>
-            <ul>
-              <li>Vitor</li>
-              <li>Lula</li>
-            </ul>
-        </div>
-      </div>
-    </div> */}
