bab2a0ccc98e3a8f9ac03ac218eeaf0f23e6b53e
rkrocha <rafaelkr7@gmail.com>
2022-02-15 18:25:32 -0300

    added CSS styling to pagination buttons

--- page/src/components/ButtonPage.js
+++ page/src/components/ButtonPage.js
@@ -4,16 +4,16 @@ import { useNavigate } from 'react-router-dom'
 const ButtonPage = (props) => {
 	const navigate = useNavigate()
 	return (
-		<>
-			{Number(props.page) > 1 && <button onClick={() =>
+		<div className='page_buttons'>
+			{Number(props.page) > 1 && <button style={{"color":"coral"}} className="input-button" onClick={() =>
 				navigate('/busca/' + props.term + '/' + props.sort + '/' + props.court + '/' + props.field + '/' + (props.page - 1))}>
 				Anterior
 			</button>}
-			{Number(props.count) / 10 > Number(props.page) && <button onClick={() =>
+			{Number(props.count) / 10 > Number(props.page) && <button style={{"color":"coral"}} className="input-button" onClick={() =>
 				navigate('/busca/' + props.term + '/' + props.sort + '/' + props.court + '/' + props.field + '/' + (Number(props.page) + 1))}>
 				Próximo
 			</button>}
-		</>
+		</div>
 	 );
 }
 
