0d74d3087d9c1fbfb397331ba8aea109c0606524
rkrocha <rafaelkr7@gmail.com>
2022-02-22 18:26:08 -0300

    implemented FastAPI

--- api/api.py
+++ api/api.py
@@ -1,20 +1,11 @@
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
+from fastapi import FastAPI
 from searchengine import SearchEngine
-
-
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 se = SearchEngine('juslite_elastic:9200')
 
-
-@app.route("/lawsuit/<path:endpoint>", methods=["GET"])
-@cross_origin()
-def query_lawsuit(endpoint):
-    return se.get_results(endpoint, request.args['sort'], request.args['court'], request.args['field'], request.args['page'])
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    app.run(host="juslite_api", port=3001)
+@app.get("//lawsuit/{term}")
+def query_lawsuit(term: str = "", sort: str = "relevante", court: str = "todos", field: str = "todos", page: int = 1):
+    return se.get_results(term, sort, court, field, page)
